4665e04c23b2f2bf37280ae55ea1b526baeb94a7
tvntvn13 <tnlmkhnn@me.com>
2023-08-28 17:46:31 +0300

    add basic component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CaptchaComponent } from './captcha/captcha.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CaptchaComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/captcha/captcha.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-captcha',
+  templateUrl: './captcha.component.html',
+  styleUrls: ['./captcha.component.css']
+})
+export class CaptchaComponent {
+
+}
